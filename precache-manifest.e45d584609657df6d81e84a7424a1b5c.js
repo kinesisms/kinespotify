5ec501fb0b24f7b6334e4d7430110e8dee84ac2c
Thien Duy <thienduymaple@gmail.com>
2019-04-06 19:09:45 +0700

    uploading

---
+++ precache-manifest.e45d584609657df6d81e84a7424a1b5c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "276f273d11f8b9dbc0a9c55bb38ea8c6",
+    "url": "/static/media/spoHome.276f273d.gif"
+  },
+  {
+    "revision": "e148270742c18d6fb8656f3a11809938",
+    "url": "/static/media/seaching.e1482707.gif"
+  },
+  {
+    "revision": "3aaaadcfdaa7c82bcee829ef629658d7",
+    "url": "/static/media/loading.3aaaadcf.gif"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1c7b8d93e9f365123442",
+    "url": "/static/js/main.1c7b8d93.chunk.js"
+  },
+  {
+    "revision": "5bd78828b3ebd01eb785",
+    "url": "/static/js/2.5bd78828.chunk.js"
+  },
+  {
+    "revision": "1c7b8d93e9f365123442",
+    "url": "/static/css/main.4061d708.chunk.css"
+  },
+  {
+    "revision": "5bd78828b3ebd01eb785",
+    "url": "/static/css/2.1621fedd.chunk.css"
+  },
+  {
+    "revision": "69a6205cbf8853f31bcbbbe84bc8a432",
+    "url": "/index.html"
+  }
+];
