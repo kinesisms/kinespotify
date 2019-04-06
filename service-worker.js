f32bf4f7d18ec347c08cb9ff62d475d53a1e0222
Thien Duy <thienduymaple@gmail.com>
2019-04-06 19:17:21 +0700

    udate ne

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e45d584609657df6d81e84a7424a1b5c.js"
+  "/kinespotify/precache-manifest.2a18ed4ef3fb4fc5802e926acdcf9232.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kinespotify/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
